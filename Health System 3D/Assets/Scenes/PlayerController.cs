8bf3d1c74d1bdbaf0a8c109f7c5e1c6a48961c26
kamilwadih <720013@student.dusd.net>
2023-12-12 14:24:13 -0800

    update

---
+++ Health System 3D/Assets/Scenes/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 movePosition;
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray movePosition = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(movePosition, out var hitInfo))
+            {
+                agent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
